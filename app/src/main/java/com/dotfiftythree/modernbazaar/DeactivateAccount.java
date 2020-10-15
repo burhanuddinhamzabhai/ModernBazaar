71c52627af14c9b04e8ae2eedddb3ea62d097fef
Burhanuddin Bariyawala <40425154+burhanuddinhamzabhai@users.noreply.github.com>
2020-10-15 18:39:54 +0530

    mbsp algo completed

---
+++ app/src/main/java/com/dotfiftythree/modernbazaar/DeactivateAccount.java
@@ -0,0 +1,14 @@
+package com.dotfiftythree.modernbazaar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeactivateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deactivate_account);
+    }
+}
--- app/src/main/java/com/dotfiftythree/modernbazaar/profile.java
+++ app/src/main/java/com/dotfiftythree/modernbazaar/profile.java
@@ -1,21 +1,30 @@
 package com.dotfiftythree.modernbazaar;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class profile extends Fragment {
 
 
-    private LinearLayout insert;
+    private LinearLayout insert,logout,deactivate,base;
+    FirebaseAuth mAuth;
     public profile() {
         // Required empty public constructor
     }
@@ -33,12 +42,41 @@ public class profile extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
         insert= v.<LinearLayout>findViewById(R.id.insertproductlin);
+        logout= v.<LinearLayout>findViewById(R.id.logoutlin);
+        deactivate=v.<LinearLayout>findViewById(R.id.deactivatelin);
+        base=v.<LinearLayout>findViewById(R.id.base);
 
+        mAuth=FirebaseAuth.getInstance();
         insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent transition = new Intent(getActivity(), InsertProduct.class);
                 startActivity(transition);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent transition = new Intent(getActivity(), MainActivity.class);
+                startActivity(transition);
+                getActivity().finish();
+            }
+        });
+        deactivate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                user.delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+
+                                }
+                            }
+                        });
             }
         });
         // Inflate the layout for this fragment
